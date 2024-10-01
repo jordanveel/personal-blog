f500266ab768d130cd761b27e896f9b939b49693
jordanveel <95485331+jordanveel@users.noreply.github.com>
2024-10-01 23:37:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+
+    menuIcon.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
